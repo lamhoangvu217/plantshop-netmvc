ec08a6634a9a7c2644f9b3bf87f36c415ac348d1
lamhoangvu217 <lamhoangvu217@gmail.com>
2021-06-28 10:45:26 +0700

    first commit

---
+++ ElectroShop/Models/MVoteLog.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+namespace ElectroShop.Models
+{
+    [Table("VoteLog")]
+    public class MVoteLog
+    {
+        [Key]
+        public int id { get; set; }
+
+        public int VoteForId { get; set; }
+        public Int16 SectionId { get; set; }
+        public String UserName { get; set; }
+        public Int16 Vote { get; set; }
+        public bool Active { get; set; }
+
+    }
+}
